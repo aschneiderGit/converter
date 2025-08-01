79e3e202302f2820c6fd050733eb08041ffa349e
aschneiderGit <aimeschneider68@gmail.com>
2025-08-01 14:17:24 +0200

    reverse button UI

--- lib/views/widgets/top_display.dart
+++ lib/views/widgets/top_display.dart
@@ -1,5 +1,6 @@
 import 'package:converter/core/theme/app_colors.dart';
 import 'package:converter/views/widgets/amount_field.dart';
+import 'package:converter/views/widgets/circle_button/icon_button.dart';
 import 'package:converter/views/widgets/currency_dropdown.dart';
 import 'package:flutter/material.dart';
 
@@ -8,38 +9,52 @@ class TopDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 2,
+    return Flexible(
+      flex: 1,
       child: Container(
-        height: double.infinity,
-        width: double.infinity,
+        padding: const EdgeInsets.only(left: 24.0, top: 12, right: 12),
         color: Theme.of(context).primary,
-        child: Container(
-          padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Flexible(flex: 3, child: AmountField()),
-                  SizedBox(width: 32),
-                  Flexible(
-                    child: SizedBox(height: 65, child: CurrencyDropdown()),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Flexible(
+                  flex: 4,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [currencyAmmountRow(), currencyAmmountRow()],
                   ),
-                ],
-              ),
-              Row(
-                children: [
-                  Flexible(flex: 3, child: AmountField()),
-                  SizedBox(width: 32),
-                  Flexible(
-                    child: SizedBox(height: 65, child: CurrencyDropdown()),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                ),
+                SizedBox(width: 12),
+                reverseButton(),
+              ],
+            ),
+            Expanded(child: Container(color: Colors.redAccent)),
+          ],
         ),
       ),
+    );
+  }
+
+  Row currencyAmmountRow() {
+    return Row(
+      children: [
+        Flexible(flex: 3, child: AmountField()),
+        SizedBox(width: 24),
+        Flexible(child: SizedBox(height: 65, child: CurrencyDropdown())),
+      ],
+    );
+  }
+}
+
+class reverseButton extends StatelessWidget {
+  const reverseButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 1,
+      child: SizedBox(child: iconButton(icon: Icons.cached, secondary: true)),
     );
   }
 }
