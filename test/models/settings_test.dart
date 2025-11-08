77587ac780409d969f81f46fe4091c884d81272b
aschneiderGit <aimeschneider68@gmail.com>
2025-11-08 16:52:27 +0100

    rede test archi

--- test/models/settings_test.dart
+++ test/models/settings_test.dart
@@ -1,7 +1,7 @@
 import 'package:test/test.dart';
 import 'package:converter/data/models/Settings.dart';
 
-import '../utils_test/date_time_test.dart';
+import '../helpers/date_time.dart';
 
 void main() {
   test('settings toMap behaviour', () {
