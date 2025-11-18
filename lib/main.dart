9efb91573773412afda5309dcf7cbb1cca0f4a4a
aschneiderGit <aimeschneider68@gmail.com>
2025-11-18 13:25:56 +0100

    remove useless import

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:converter/data/databases/database_helper.dart';
 import 'package:converter/providers/converter_provider.dart';
 import 'package:converter/views/home.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
